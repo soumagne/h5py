f0dc752bddfdbd1fc0852bc7ba0c88fe5bc154e8
Aleksandar Jelenak <ajelenak@hdfgroup.org>
2014-02-15 20:01:38 -0600

    Fix one line's inconsistent indentation in h5py/h5f.pyx

--- h5py/h5f.pyx
+++ h5py/h5f.pyx
@@ -466,7 +466,7 @@ cdef class FileID(GroupID):
               """
 
               with _objects.registry.lock:
-                 self.locked = False
+                  self.locked = False
                   H5Fclose_ff(self.id, esid_default(es_id))
                   _objects.registry.cleanup()
 
