5a5405efcc655399b5176d6d0c2d06a8b983fb1c
Aleksandar Jelenak <ajelenak@hdfgroup.org>
2014-02-19 09:21:47 -0600

    First versions of Python high-level modules for HDF5 RC and TR APIs

---
+++ h5py/_hl/transaction.py
@@ -0,0 +1,30 @@
+"""
+Python high-level interface for the H5TR API
+"""
+
+from h5py import h5, version, h5tr
+
+# Check if this module can be used
+mpi = h5.get_config().mpi
+hdf5_ver = version.hdf5_version_tuple[0:3]
+if not mpi or hdf5_ver < (1, 9, 170):
+    raise RuntimeError('This module requires h5py to be built with MPI and HDF5 '
+                       'FastForward library')
+
+class Transaction:
+    """
+    Represents an HDF5 FastForward transaction.
+    """
+
+    def __init__(self, f, rc, tnum):
+        self._id = h5tr.create(f.id, rc.id, tnum)
+
+
+    @property
+    def id(self):
+        """ Returns transaction ID"""
+        return self._id
+
+
+    def close(self):
+        self._id._close()
