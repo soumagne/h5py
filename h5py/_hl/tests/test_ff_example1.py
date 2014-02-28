b7636d848255682690ad08bed62dbb1c27a11b40
Aleksandar Jelenak <ajelenak@hdfgroup.org>
2014-02-28 15:50:54 -0600

    Cleaning up common_ff.py and test_ff_example1.py

--- h5py/_hl/tests/test_ff_example1.py
+++ h5py/_hl/tests/test_ff_example1.py
@@ -18,30 +18,19 @@ class BaseTest(TestCase_ff):
     def setUp(self):
         self.ff_cleanup()
         self._old_dir = os.getcwd()
-        print "old dir = %s" % self._old_dir
-        os.chdir(self._exe_dir())
+        os.chdir(self.exe_dir)
         #self.run_h5ff_server()
-        print "passed run_h5ff_server()"
 
 
     def tearDown(self):
         self.ff_cleanup()
         os.chdir(self._old_dir)
-        print "back in = %s" % os.getcwd()
 
 
 class TestSimple(BaseTest):
 
     def test_mpi_thread_multi(self):
         """ MPI_THREAD_MULTIPLE support """
-        print "In test..."
-        print "cwd = %s" % os.getcwd()
-
-        print "before import MPI"
         from mpi4py import MPI
-        print "after import MPI"
-        print "Is initialized?", MPI.Is_initialized()
-        print "before MPI.Init_thread()"
         provided = MPI.Init_thread(required=MPI.THREAD_MULTIPLE)
-        print "after MPI.Init_thread()"
         self.assertEqual(provided, MPI.THREAD_MULTIPLE)
