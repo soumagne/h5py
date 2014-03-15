d7a98c895c611e65c29f44de5e18b54ca89cd64a
Aleksandar Jelenak <ajelenak@hdfgroup.org>
2014-03-14 20:36:34 -0500

    Example 2 as a test

--- h5py/_hl/tests/test_ff_example1.py
+++ h5py/_hl/tests/test_ff_example1.py
@@ -78,7 +78,7 @@ class TestWorkEnv(BaseTest):
 class TestExample1(BaseTest):
 
     def test_example1(self):
-        """ Example1 """
+        """ Example 1 """
         from mpi4py import MPI
         comm = MPI.COMM_WORLD
         eff_init(comm, MPI.INFO_NULL)
@@ -89,4 +89,41 @@ class TestExample1(BaseTest):
 
 
     def test_example2(self):
-        pass
+        """ Example 2 """
+        from mpi4py import MPI
+        from h5py import h5p
+
+        comm = MPI.COMM_WORLD
+        eff_init(comm, MPI.INFO_NULL)
+        my_rank = comm.Get_rank()
+        es = EventStack()
+        f = File('ff_file_ex1.h5', 'w', driver='iod', comm=comm,
+                 info=MPI.INFO_NULL)
+        my_version = 0
+        version = f.acquire_context(my_version)
+        self.assertEqual(my_version, version)
+        
+        comm.Barrier()
+        
+        if my_rank == 0:
+            f.create_transaction(1)
+            f.tr.start(h5p.DEFAULT)
+
+            grp1 = f.create_group("G1", f.tr)
+            grp2 = grp1.create_group("G2", f.tr)
+
+            f.tr.finish()
+            # f.tr._close()
+        
+        f.rc.release()
+        
+        comm.Barrier()
+        
+        if my_rank == 0:
+            grp1.close()
+            grp2.close()
+        #f.rc._close()
+        f.close()
+        #es.close()
+        #comm.Barrier()
+        eff_finalize()
