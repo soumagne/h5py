e0b201b73cad517e239eb55bafc8adbbb2386a95
Aleksandar Jelenak <ajelenak@hdfgroup.org>
2014-06-14 19:47:53 -0600

    sanbox.py update

--- eff/sandbox.py
+++ eff/sandbox.py
@@ -13,7 +13,8 @@ sys.path.insert(1, h5py_dir)
 
 from mpi4py import MPI
 from h5py.eff_control import eff_init, eff_finalize
-from h5py import h5, h5p, File
+from h5py import h5, h5p, File, Dataset
+import numpy as np
 
 comm = MPI.COMM_WORLD
 eff_init(comm, MPI.INFO_NULL)
@@ -35,28 +36,9 @@ if my_rank == 0:
     f.create_transaction(2)
     f.tr.start()
 
-    print ">>>>>>>"
-    print "f.ctn =", f.ctn
-    print "f.tr =", f.tr
-    print "f.rc =", f.rc
-    print "f.es =", f.es
-    print "<<<<<<"
-
-    grp1 = f.create_group("G1")
-    print ">>>>>>>"
-    print "grp1.ctn =", grp1.ctn
-    print "grp1.tr =", grp1.tr
-    print "grp1.rc =", grp1.rc
-    print "grp1.es =", grp1.es
-    print "<<<<<<"
-
-    grp2 = grp1.create_group("G2")
-    print ">>>>>>>"
-    print "grp2.ctn =", grp2.ctn
-    print "grp2.tr =", grp2.tr
-    print "grp2.rc =", grp2.rc
-    print "grp2.es =", grp2.es
-    print "<<<<<<"
+    dset = f.create_dataset('foo', ())
+    assert isinstance(dset, Dataset)
+    assert dset.shape == ()
 
     f.tr.finish()
 #    f.tr.close()
@@ -67,8 +49,8 @@ f.rc.release()
 comm.Barrier()
 
 if my_rank == 0:
-    grp1.close()
-    grp2.close()
+    dset.close()
+    # grp2.close()
 
 ##############
 #f.rc.close()
