dad129ae795c2f38c1bb3159b82d3ceace2afb75
Aleksandar Jelenak <ajelenak@hdfgroup.org>
2014-06-27 12:09:09 -0700

    More view tests

--- h5py/_hl/tests/test_ff_view.py
+++ h5py/_hl/tests/test_ff_view.py
@@ -27,7 +27,7 @@ class TestView(BaseTest):
         Feature: View (H5V) operations
     """
 
-    # @ut.skip('Test works')
+    @ut.skip('Test works')
     def test_view_001(self):
         """ View creation on file, group, dataset, attribute"""
         from mpi4py import MPI
@@ -53,9 +53,56 @@ class TestView(BaseTest):
             v1 = View(f, q)
             self.assertIsInstance(v1, View)
             v2 = View(g, q)
+            self.assertIsInstance(v2, View)
             v3 = View(d, q)
+            self.assertIsInstance(v3, View)
             with self.assertRaises(TypeError):
                 v4 = View(f.attrs['a'], q)
+
+            f.rc.release()
+
+            d.close()
+            g.close()
+            f.close()
+        eff_finalize()
+
+
+    @ut.skip('Test works')
+    def test_view_002(self):
+        """ View location object"""
+        from h5py.highlevel import Group, Dataset
+        from mpi4py import MPI
+        comm = MPI.COMM_WORLD
+        eff_init(comm, MPI.INFO_NULL)
+        rank = comm.Get_rank()
+        if rank == 0:
+            f = File(self.fname, 'w', driver='iod', comm=comm,
+                     info=MPI.INFO_NULL)
+            f.acquire_context(1)
+            f.create_transaction(2)
+            f.tr.start()
+
+            g = f.create_group('G')
+            d = g.create_dataset('D', (10,))
+
+            f.tr.finish()
+            f.rc.release()
+            f.acquire_context(2)
+
+            q = AQuery('link_name') == 'foo'
+            v1 = View(f, q)
+            v2 = View(g, q)
+            v3 = View(d, q)
+
+            # This is an issue with how to create a new file instance from
+            # FileID object.
+            # self.assertIsInstance(v1.location(), File)
+            obj = v2.location()
+            self.assertIsInstance(obj, Group)
+            obj.close()
+            obj = v3.location()
+            self.assertIsInstance(obj, Dataset)
+            obj.close()
 
             f.rc.release()
 
